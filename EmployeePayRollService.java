f1894be0db5deac4008ebcb43eabc2ca5b5b4774
komal@komal405.com <ankita80kadam@gmail.com>
2021-05-10 19:35:52 +0530

    ADD:create an employee payroll service to read and write

--- EmployeePayRollService.java
+++ EmployeePayRollService.java
@@ -1,7 +1,60 @@
-class EmployeePayRollService
-{
-public static void main(String args[])
-{
-System.out.println("Welcome to EmployeePayRollService");
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmployeePayrollService {
+
+	private List<EmployeePayrollData> datas;
+	public EmployeePayrollService()
+	{
+		
+	}
+	public EmployeePayrollService(List<EmployeePayrollData> datas)
+	{
+		this.datas=datas;
+	}
+	public static void main(String arggs[])
+	{
+		List<EmployeePayrollData> datas = new ArrayList<EmployeePayrollData>();
+		EmployeePayrollService service = new EmployeePayrollService(datas);
+		service.readData(new Scanner(System.in));
+		service.writeData();
+	}
+	public void writeData()
+	{
+		System.out.println("\nWrinting Employee payroll  : \n"+datas);
+	}
+	
+	private void readData(Scanner scanner)
+	{
+		System.out.println("Enter id");
+		int id = scanner.nextInt();
+		
+		System.out.println("Enter name");
+		String name = scanner.next();
+		
+		System.out.println("Enter salary");
+		double salary = scanner.nextDouble();
+		this.datas.add(new EmployeePayrollData(id,salary,name));
+		
+	}
 }
+
+class EmployeePayrollData
+{
+	private int id;
+	private String name;
+	private double salary;
+	
+	public EmployeePayrollData(int id,double salary,String name)
+	{
+	this.id=id;
+	this.name=name;
+	this.salary=salary;
+}
+	@Override
+	public String toString()
+	{
+		return "id="+ id + " ,name =' "+name + '\'' + ",salary="+salary;
+	}
 }
